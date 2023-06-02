71a257c4368dc72cd282bc73b21cb41ee79cac82
Mark Pushkar <mark.pushkar92@gmail.com>
2023-06-02 17:25:22 +0700

    Initial Commit

---
+++ GameOfSet/GameOfSetApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfSetApp.swift
+//  GameOfSet
+//
+//  Created by Марк Пушкарь on 02.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct GameOfSetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
